49b850f79c2e923589fd5fd2bf07f47245bc344e
Maria-Anastasia Moustaka <mariamou7@windowslive.com>
2022-10-27 15:15:53 +0300

    Create line_follow.py
    Line follow

---
+++ Maqueen/line_follow.py
@@ -0,0 +1,20 @@
+def on_forever():
+    if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
+        maqueen.motor_run(maqueen.Motors.ALL, maqueen.Dir.CW, 50)
+    elif maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
+        maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        maqueen.motor_stop(maqueen.Motors.M1)
+        if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
+            maqueen.motor_run(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+            maqueen.motor_stop(maqueen.Motors.M1)
+    elif maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
+        maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.motor_stop(maqueen.Motors.M2)
+        if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 1 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 1:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+            maqueen.motor_stop(maqueen.Motors.M2)
+        if maqueen.read_patrol(maqueen.Patrol.PATROL_LEFT) == 0 and maqueen.read_patrol(maqueen.Patrol.PATROL_RIGHT) == 0:
+            maqueen.motor_run(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        else:
+            maqueen.motor_stop(maqueen.Motors.M2)
+basic.forever(on_forever)
